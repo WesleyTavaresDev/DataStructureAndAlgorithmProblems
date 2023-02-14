d397f1f77a46d29ddd6d817b54d6c440faa73cf2
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-02-14 19:23:03 -0300

    Tests written

--- List/running_total_test.go
+++ List/running_total_test.go
@@ -1,1 +1,29 @@
 package list
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTotal(t *testing.T) {
+
+	elements := []int{2, 4, 6, 8, 20}
+
+	total := RunTotal(elements)
+	want := []int{2, 6, 12, 20, 40}
+
+	if reflect.DeepEqual(want, total) {
+		t.Errorf("Wanting %d, received %d", want, total)
+	}
+}
+
+func TestTotalNegative(t *testing.T) {
+	elements := []int{-2, -4, -6, -8, 20}
+
+	total := RunTotal(elements)
+	want := []int{-2, -6, -12, -20, 0}
+
+	if reflect.DeepEqual(want, total) {
+		t.Errorf("Wanting %d, received %d", want, total)
+	}
+}
