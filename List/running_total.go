690f19b5895e68cfee1c415a8bd6f8d0476957dc
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-02-14 19:53:58 -0300

    RunTotal function

--- List/running_total.go
+++ List/running_total.go
@@ -2,5 +2,13 @@ package list
 
 func RunTotal(elements []int) []int {
 
+	if len(elements) == 1 {
+		return elements
+	}
+
+	for i := 1; i < len(elements); i++ {
+		elements[i] += elements[i-1]
+	}
+
 	return elements
 }
