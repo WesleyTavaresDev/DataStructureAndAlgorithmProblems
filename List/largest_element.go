bd6c17194743223bebc765c67b39495af306da38
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-02-14 02:42:13 -0300

    LargestElement test

--- List/largest_element.go
+++ List/largest_element.go
@@ -2,10 +2,8 @@ package list
 
 // Write a function that returns the largest element in a list.
 
-func LargestElement() int {
+func LargestElement(arr []int) int {
 	largest := 0
-
-	arr := []int{3, 10, 20, 30, 1, 4, 25, 0, 5, -1}
 
 	for i := 0; i < len(arr); i++ {
 
--- List/largest_element_test.go
+++ List/largest_element_test.go
@@ -1,1 +1,13 @@
 package list
+
+import "testing"
+
+func TestLargest(t *testing.T) {
+	shouldReturn25 := [...]int{0, 0, 9, 1, 25, 6, 13}
+	want := 25
+	returned := LargestElement(shouldReturn25[:])
+
+	if returned != want {
+		t.Errorf("result %d, waiting %d", returned, want)
+	}
+}
