d03e4f386b113e7918ef9352a1a7645311441acf
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-02-14 21:31:18 -0300

    Selection ascending passed

--- Sorting/Selection/selection_sort.go
+++ Sorting/Selection/selection_sort.go
@@ -2,5 +2,16 @@ package selection
 
 func SelectionSort(elements []int) []int {
 
+	for i := 0; i < len(elements)-1; i++ {
+		minIndex := i
+
+		for j := i + 1; j <= len(elements)-1; j++ {
+			if elements[j] < elements[minIndex] {
+				minIndex = j
+			}
+		}
+		elements[i], elements[minIndex] = elements[minIndex], elements[i]
+	}
+
 	return elements
 }
