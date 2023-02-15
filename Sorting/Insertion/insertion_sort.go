8eac7e0699ba0928e529329b6195e48e95e78779
Wesley Tavares <wesleyTavaresDev@outlook.com>
2023-02-15 19:56:34 -0300

    insertion_sort algorithm

--- Sorting/Insertion/insertion_sort.go
+++ Sorting/Insertion/insertion_sort.go
@@ -2,5 +2,13 @@ package insertion
 
 func Insertion(elements []int) []int {
 
+	for i := 1; i < len(elements); i++ {
+		for j := i; j > 0; j-- {
+			if elements[j] < elements[j-1] {
+				elements[j], elements[j-1] = elements[j-1], elements[j]
+			}
+		}
+	}
+
 	return elements
 }
