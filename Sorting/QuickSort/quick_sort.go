00909f11f2d1051388f25347dd139669d550b0f7
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2023-02-18 19:32:00 -0300

    quick sort algorithm

--- Sorting/QuickSort/quick_sort.go
+++ Sorting/QuickSort/quick_sort.go
@@ -1,10 +1,27 @@
 package quicksort
 
-func partition(arr []int, low, high int) int {
+func partition(arr []int, left, right int) int {
+	pivot := arr[left]
+	i := left
 
-	return 0
+	for j := left + 1; j <= right; j++ {
+		if arr[j] <= pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+
+	arr[left], arr[i] = arr[i], arr[left]
+
+	return i
 }
 
-func Quicksort(arr []int, low, high int) {
-	partition(arr, low, high)
+func Quicksort(arr []int, left, right int) {
+
+	if left < right {
+		index_pivot := partition(arr, left, right)
+		Quicksort(arr, left, index_pivot-1)
+		Quicksort(arr, index_pivot+1, right)
+	}
+
 }
--- Sorting/QuickSort/quick_sort_test.go
+++ Sorting/QuickSort/quick_sort_test.go
@@ -7,13 +7,13 @@ import (
 
 func TestQuickSort(t *testing.T) {
 
-	arr := []int{5, 8, 6, 2, 0, 78, 10, 11, 3, 54}
+	elements := []int{5, 8, 6, 2, 0, 78, 10, 11, 3, 54}
 
-	Quicksort(arr, 0, len(arr)-1)
+	Quicksort(elements, 0, len(elements)-1)
 
 	want := []int{0, 2, 3, 5, 6, 8, 10, 11, 54, 78}
 
-	if !reflect.DeepEqual(want, arr) {
-		t.Errorf("Wanted %d, received %d", want, arr)
+	if !reflect.DeepEqual(want, elements) {
+		t.Errorf("Wanted %d, received %d", want, elements)
 	}
 }
