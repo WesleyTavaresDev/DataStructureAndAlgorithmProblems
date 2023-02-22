46509679ea6d6a1d67bdcf507465439e0c72bba7
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2023-02-22 20:51:16 -0300

    Test Big Linear Search added

--- Searching/LinearSeach/linear_search_test.go
+++ Searching/LinearSeach/linear_search_test.go
@@ -27,3 +27,14 @@ func TestLinearSearchNotFound(t *testing.T) {
 		t.Errorf("Want %d, received %d", want, targetIndex)
 	}
 }
+
+func TestBigLinearSearch(t *testing.T) {
+	arr := []int{100, 98, 11, 61, 30, 47, 42, 77, 54, 91, 99, 52, 95, 49, 45, 15, 23, 3, 60, 72, 102, 59, 44, 31, 51, 41}
+	targetIndex := LinearSearch(arr, 41)
+
+	want := len(arr) - 1
+
+	if !reflect.DeepEqual(want, targetIndex) {
+		t.Errorf("Want %d, received %d", want, targetIndex)
+	}
+}
