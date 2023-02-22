b76905a9c0dfb9165d4a8f7528b0db8784e731d9
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2023-02-22 20:22:01 -0300

    linear search go file created

---
+++ Searching/LinearSeach/linear_search.go
@@ -0,0 +1,10 @@
+package linearseach
+
+func LinearSearch(arr []int, target int) (index int) {
+
+	//TODO: Iterate the slice incrementing the index
+	//TODO: returns index if the current element == target
+	//TODO: Else return -1
+
+	return -1
+}
