77435f4ea4d2a0c38679e95767759581bd335db5
WesleyTavaresDev <wesleyTavaresDev@outlook.com>
2023-02-22 20:32:50 -0300

    linear search test created

---
+++ Searching/LinearSeach/linear_search_test.go
@@ -0,0 +1,17 @@
+package linearseach
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLinearSearch(t *testing.T) {
+	arr := []int{100, 98, 11, 61, 30, 47, 42, 77, 54, 91, 99}
+
+	targetIndex := LinearSearch(arr, 99)
+	want := 10
+
+	if !reflect.DeepEqual(want, targetIndex) {
+		t.Errorf("Want %d, received %d", want, targetIndex)
+	}
+}
